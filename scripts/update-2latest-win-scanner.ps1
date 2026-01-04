204001e7814178106daf4ea89bf216efe7b3924d
danziger-e <eduard.danziger@t-online.de>
2026-01-04 19:54:57 +0100

    update module version listing to use direct proxy

--- scripts/update-2latest-win-scanner.ps1
+++ scripts/update-2latest-win-scanner.ps1
@@ -5,7 +5,7 @@ $mod = 'github.com/eduarddanziger/sound-win-scanner/v4'
 if (-not (Get-Command go -ErrorAction SilentlyContinue)) { throw 'go not found in PATH' }
 
 # list published module versions
-$all = (go list -m -versions $mod) -split '\s+'
+$all = (go list -m -versions -proxy=direct $mod) -split '\s+'
 
 # no versions -> nothing to pick
 if (-not $all -or $all.Count -lt 2) { throw "no versions returned for $mod" }
