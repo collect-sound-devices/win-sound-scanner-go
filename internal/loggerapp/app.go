9db17bfb29f3a1ebfd8e7e7f922261394987f71a
rangular <eduarddanziger@outlook.com>
2026-02-01 23:10:13 +0100

    add logger and scanner applications with device change and volume notification handlers

--- internal/loggerapp/app.go
+++ internal/loggerapp/app.go
@@ -1,4 +1,4 @@
-package cliapp
+package loggerapp
 
 import (
 	"context"
--- scripts/build.ps1
+++ scripts/build.ps1
@@ -10,9 +10,11 @@ $Env:CGO_ENABLED = "1"
 $Env:CC = "E:\tools\llvm-mingw\bin\x86_64-w64-mingw32-clang.exe"
 $Env:CXX = "E:\tools\llvm-mingw\bin\x86_64-w64-mingw32-clang++.exe"
 
-go build -o (Join-Path $PWD.Path 'bin/')
+go build -o (Join-Path $PWD.Path 'bin/win-sound-logger.exe') ./cmd/win-sound-logger
+go build -o (Join-Path $PWD.Path 'bin/win-sound-scanner.exe') ./cmd/win-sound-scanner
 
 .\scripts\internal\fetch-native.ps1
 
 ## once more
-go build -o (Join-Path $PWD.Path 'bin/')
+go build -o (Join-Path $PWD.Path 'bin/win-sound-logger.exe') ./cmd/win-sound-logger
+go build -o (Join-Path $PWD.Path 'bin/win-sound-scanner.exe') ./cmd/win-sound-scanner
