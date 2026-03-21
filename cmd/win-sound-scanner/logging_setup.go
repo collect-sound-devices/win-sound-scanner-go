ad1f9ee47530fda3ec4dfcb9a604a59e982841a0
dan-chemo <eduarddanziger@t-online.de>
2026-03-21 08:17:49 +0100

    Refactor: Improve and unify log formatting

--- cmd/win-sound-scanner/logging_setup.go
+++ cmd/win-sound-scanner/logging_setup.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log/slog"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/sys/windows"
 )
+
+const appLogTimeLayout = "2006-01-02 15:04:05.000000"
+
+type appLogHandler struct {
+	mu     *sync.Mutex
+	writer io.Writer
+	level  slog.Leveler
+	attrs  []slog.Attr
+}
 
 // newAppLogger builds a structured app logger.
 func newAppLogger(writer io.Writer) *slog.Logger {
 	if writer == nil {
 		panic("nil writer")
 	}
-	return slog.New(
-		slog.NewTextHandler(
-			writer,
-			&slog.HandlerOptions{Level: slog.LevelInfo}))
+	return slog.New(&appLogHandler{
+		mu:     &sync.Mutex{},
+		writer: writer,
+		level:  slog.LevelInfo,
+	})
 }
 
 func fatalLog(logger *slog.Logger, message string, args ...any) {
@@ -23,4 +41,140 @@ func fatalLog(logger *slog.Logger, message string, args ...any) {
 	}
 	logger.Error(message, args...)
 	os.Exit(1)
+}
+
+func (h *appLogHandler) Enabled(_ context.Context, level slog.Level) bool {
+	minLevel := slog.LevelInfo
+	if h.level != nil {
+		minLevel = h.level.Level()
+	}
+	return level >= minLevel
+}
+
+func (h *appLogHandler) Handle(_ context.Context, record slog.Record) error {
+	if !h.Enabled(context.Background(), record.Level) {
+		return nil
+	}
+
+	timestamp := record.Time
+	if timestamp.IsZero() {
+		timestamp = time.Now()
+	}
+
+	var builder strings.Builder
+	builder.Grow(128)
+	builder.WriteString(timestamp.Local().Format(appLogTimeLayout))
+	builder.WriteByte(' ')
+	builder.WriteString(appLogLevel(record.Level))
+	builder.WriteString(" [")
+	builder.WriteString(strconv.FormatUint(uint64(windows.GetCurrentThreadId()), 10))
+	builder.WriteString("] ")
+	builder.WriteString(record.Message)
+
+	h.appendAttrs(&builder, h.attrs)
+	record.Attrs(func(attr slog.Attr) bool {
+		h.appendAttr(&builder, attr)
+		return true
+	})
+	builder.WriteByte('\n')
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	_, err := io.WriteString(h.writer, builder.String())
+	return err
+}
+
+func (h *appLogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	clone := h.clone()
+	clone.attrs = append(clone.attrs, attrs...)
+	return clone
+}
+
+func (h *appLogHandler) WithGroup(name string) slog.Handler {
+	return h.clone()
+}
+
+func (h *appLogHandler) clone() *appLogHandler {
+	attrs := make([]slog.Attr, len(h.attrs))
+	copy(attrs, h.attrs)
+	return &appLogHandler{
+		mu:     h.mu,
+		writer: h.writer,
+		level:  h.level,
+		attrs:  attrs,
+	}
+}
+
+func (h *appLogHandler) appendAttrs(builder *strings.Builder, attrs []slog.Attr) {
+	for _, attr := range attrs {
+		h.appendAttr(builder, attr)
+	}
+}
+
+func (h *appLogHandler) appendAttr(builder *strings.Builder, attr slog.Attr) {
+	attr.Value = attr.Value.Resolve()
+	if attr.Equal(slog.Attr{}) || attr.Key == "" {
+		return
+	}
+	if attr.Value.Kind() == slog.KindGroup {
+		for _, child := range attr.Value.Group() {
+			h.appendAttr(builder, child)
+		}
+		return
+	}
+	builder.WriteByte(' ')
+	builder.WriteString(attr.Key)
+	builder.WriteByte('=')
+	appendAttrValue(builder, attr.Value)
+}
+
+func appendAttrValue(builder *strings.Builder, value slog.Value) {
+	switch value.Kind() {
+	case slog.KindString:
+		builder.WriteString(strconv.Quote(value.String()))
+	case slog.KindInt64:
+		builder.WriteString(strconv.FormatInt(value.Int64(), 10))
+	case slog.KindUint64:
+		builder.WriteString(strconv.FormatUint(value.Uint64(), 10))
+	case slog.KindFloat64:
+		builder.WriteString(strconv.FormatFloat(value.Float64(), 'f', -1, 64))
+	case slog.KindBool:
+		builder.WriteString(strconv.FormatBool(value.Bool()))
+	case slog.KindDuration:
+		builder.WriteString(strconv.Quote(value.Duration().String()))
+	case slog.KindTime:
+		builder.WriteString(strconv.Quote(value.Time().Format(time.RFC3339Nano)))
+	case slog.KindAny:
+		appendAnyValue(builder, value.Any())
+	default:
+		builder.WriteString(strconv.Quote(value.String()))
+	}
+}
+
+func appendAnyValue(builder *strings.Builder, value any) {
+	switch v := value.(type) {
+	case nil:
+		builder.WriteString("<nil>")
+	case error:
+		builder.WriteString(strconv.Quote(v.Error()))
+	case fmt.Stringer:
+		builder.WriteString(strconv.Quote(v.String()))
+	case string:
+		builder.WriteString(strconv.Quote(v))
+	default:
+		builder.WriteString(fmt.Sprint(v))
+	}
+}
+
+func appLogLevel(level slog.Level) string {
+	switch {
+	case level <= slog.LevelDebug:
+		return "D"
+	case level >= slog.LevelError:
+		return "E"
+	case level >= slog.LevelWarn:
+		return "W"
+	default:
+		return "I"
+	}
 }
